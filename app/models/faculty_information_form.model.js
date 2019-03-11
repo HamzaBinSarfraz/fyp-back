cb65eb3c22a9268d970c869e05bab74b2d173a98
hamzabinsarfraz <ameer.hamza@logicps.com>
2019-03-12 00:56:35 +0500

    asjkdjkasdhajkdhasjkd

--- app/models/faculty_information_form.model.js
+++ app/models/faculty_information_form.model.js
@@ -5,55 +5,40 @@ var Faculty = mongoose.Schema({
 
     institution: String,
     program: String,
-
-
-    fullTimeFaculty: [{
         facultySize: String, 
         noOfFaculty: String, 
         fullProfrssor: String,
         associateProfessor: String,
         assistantProfessor: String,
         leacures: String,
-        teaching: String
-    }], 
-    partTimeFaculty: [{
-        facultySize: String, 
-        noOfFaculty: String, 
-        fullProfrssor: String,
-        associateProfessor: String,
-        assistantProfessor: String,
-        leacures: String,
-        teaching: String
-    }], 
-    fullTimeFacultyStability2011: [{
+        teaching: String,
+        facultySize1: String, 
+        noOfFaculty1: String, 
+        fullProfrssor1: String,
+        associateProfessor1: String,
+        assistantProfessor1: String,
+        leacures1: String,
+        teaching1: String,
         totalFaculty: String, 
         resigned: String, 
         retired: String,
         terminated: String,
-        newInduction: String
-    }], 
-    fullTimeFacultyStability2012: [{
-        totalFaculty: String, 
-        resigned: String, 
-        retired: String,
-        terminated: String,
-        newInduction: String
-    }], 
-    fullTimeFacultyStability2013: [{
-        totalFaculty: String, 
-        resigned: String, 
-        retired: String,
-        terminated: String,
-        newInduction: String
-    }], 
-    fullTimeFacultyStability2014: [{
-        totalFaculty: String, 
-        resigned: String, 
-        retired: String,
-        terminated: String,
-        newInduction: String
-    }], 
-
+        newInduction: String,
+        totalFaculty1: String, 
+        resigned1: String, 
+        retired1: String,
+        terminated1: String,
+        newInduction1: String,
+        totalFaculty2: String, 
+        resigned2: String, 
+        retired2: String,
+        terminated2: String,
+        newInduction2: String,
+        totalFaculty3: String, 
+        resigned3: String, 
+        retired3: String,
+        terminated3: String,
+        newInduction3: String
 }, {
     timestamp: true
 });
