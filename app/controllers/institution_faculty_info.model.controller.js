6c3c93588db8ba06fa8662afe803027061f3cd09
hamzabinsarfraz <ameer.hamza@logicps.com>
2019-04-04 23:45:51 +0500

    asddjhjabsdasbdabidubauibdaisubdiuabsd

--- app/controllers/institution_faculty_info.model.controller.js
+++ app/controllers/institution_faculty_info.model.controller.js
@@ -3,15 +3,103 @@ const InstitutionFaculty = require("../models/institution_faculty_info.model");
 exports.create = (req, res) => {
     var newInstitution = new InstitutionFaculty(req.body);
     newInstitution.save()
-    .then(data => {
+        .then(data => {
+            return res.status(200).send({
+                status: true,
+                data: data
+            })
+        })
+        .catch(err => {
+            return res.status(200).send({
+                status: false,
+                message: err.message
+            })
+        })
+}
+
+
+exports.findI = (req, res) => {
+    InstitutionFaculty.find()
+        .then(data => {
+            return res.status(200).send({
+                status: true,
+                data: data
+            })
+        })
+        .catch(err => {
+            return res.status(200).send({
+                status: false,
+                message: err.message
+            })
+        })
+}
+
+exports.findId = (req, res) => {
+    InstitutionFaculty.findOne({
+            _id: req.params.id
+        })
+        .then(data => {
+            return res.status(200).send({
+                status: true,
+                data: data
+            })
+        })
+        .catch(err => {
+            return res.status(200).send({
+                status: false,
+                message: err.message
+            })
+        })
+}
+
+exports.updateInstitute = (req, res) => {
+    InstitutionFaculty.update({
+        _id: req.params.id
+    }, {
+        $set: {
+            university: req.body.university,
+            campus: req.body.campus,
+            date: req.body.date,
+            sr_no: req.body.sr_no,
+            cnic: req.body.cnic,
+            nameOfFacultyMember: req.body.nameOfFacultyMember,
+            gender: req.body.gender,
+            regNo: req.body.regNo,
+            degree: req.body.degree,
+            creadit: req.body.creadit,
+            dateOfAward: req.body.dateOfAward,
+            nameOfUni: req.body.nameOfUni,
+            country: req.body.country,
+            fullTime: req.body.fullTime
+        }
+    }
+    ).then(data => {
         return res.status(200).send({
-            status: true, 
-            data: data
+            status: true,
+            message: "Updated ... "
         })
     })
     .catch(err => {
         return res.status(200).send({
-            status: false, 
+            status: false,
+            message: err.message
+        })
+    })
+}
+
+exports.deleteI = (req, res) => {
+    InstitutionFaculty.deleteOne({
+        _id: req.params.id
+    })
+    .then(data => {
+        return res.status(200).send({
+            status: true,
+            message: "deleted ... "
+        })
+    })
+    .catch(err => {
+        return res.status(200).send({
+            status: false,
             message: err.message
         })
     })
--- app/routes/institution_faculty_info.model.route.js
+++ app/routes/institution_faculty_info.model.route.js
@@ -2,6 +2,10 @@ const bla = require("../../utility/verify_token.middleware");
 
 module.exports = app => {
     const Institute = require("../controllers/institution_faculty_info.model.controller");
-
+    
     app.post("/institution_faculty_info", bla.jwtVerify, Institute.create);
+    app.get('/institution_faculty_info', bla.jwtVerify, Institute.findI);
+    app.get('/institution_faculty_info/:id', bla.jwtVerify, Institute.findId);
+    app.put('/institution_faculty_info/:id', bla.jwtVerify, Institute.updateInstitute);
+    app.delete('/institution_faculty_info/:id', bla.jwtVerify, Institute.deleteI);
 };
